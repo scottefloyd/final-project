e9303744ba3f61b8a40cfbf325ce8338b0b6d3db
steven <1stevenly92@gmail.com>
2019-06-21 10:23:16 -0400

    fixed the merge nonsense

--- src/app/audience/audience.component.ts
+++ src/app/audience/audience.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from "../chat.service";
-import { GoogleChartComponent } from 'angular-google-charts';
 
 @Component({
   selector: 'audience',
@@ -9,72 +8,16 @@ import { GoogleChartComponent } from 'angular-google-charts';
 })
 export class AudienceComponent implements OnInit {
 
-
-  // 
-
-  messages: any[] =[];
-
-  message: any;
-
-
-  myData: any[] = [];
-
-  myData2: any[] =[];
-
-  myColumnNames =  ['Category', 'Score'];
-    
-  myOptions = {
-    colors: ['#e0443e', '#a6693e', '#f3b49f']
-  };
-
-  
-  myType = "BarChart";
- 
-
-  
+  messages: string[] = [];
   playerReady: any = false;
+  message: any;
   gameOver: any;
   allPlayerScores: [];
-
 
   constructor(private chatService: ChatService) { }
 
   ngOnInit() {
     this.chatService.getMessages().subscribe(message => {
-
-      // console.log(message);
-      this.messages.push(message);
-      // console.log(this.messages);
-
-      this.myData = [
-        ['Style', this.messages[0].style],
-        ['Skill', this.messages[0].skill],
-        ['Originality', this.messages[0].originality],
-        ['Effort', this.messages[0].effort],
-       [ 'Average', this.messages[0].average],
-       [  'Total', this.messages[0].total]
-    
-      ];
-
-      // this.myData2 = [
-      //   ['Style', this.messages[1].style],
-      //   ['Skill', this.messages[1].skill],
-      //   ['Originality', this.messages[1].originality],
-      //   ['Effort', this.messages[1].effort]
-    
-      // ];
-
-      // console.log(this.messages[0].skill)
-    
-       
-
-      // console.log(data);
-    
-      
-    
-      
-    });
-
       this.messages = message;
       if (this.messages) {
         this.playerReady = true;
